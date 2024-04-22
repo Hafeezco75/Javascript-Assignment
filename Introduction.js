adef90399e29e52167b04b578a313e5d6ad49317
Hafeezco75 <hafeezco75@gmail.com>
2024-04-22 08:08:36 -0700

    Adding new files to Javascript repository

---
+++ Introduction.js
@@ -0,0 +1,18 @@
+var a = 1;
+console.log(a);
+var a = 2;
+console.log(a);
+
+let var1 = 2;
+let var2 = 3;
+let var3 = 5;
+var temp;
+
+temp = var1;
+var1 = var2;
+var2 = var3;
+var3 = temp;
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
---
+++ tasks.js
@@ -0,0 +1,30 @@
+let x = 3;
+let y = 4;
+
+let output = 2 * (x + y);
+console.log(output)
+
+let b = 16;
+let mod = b % 10;
+let add = mod + b;
+
+console.log(mod)
+console.log(add)
+
+x;
+let n = 5;
+let calc = x * 4 * n;
+console.log(calc)
+
+let a = 36;
+let x1 = a + 10;
+let y1 = a - 10;
+let z = a % 10;
+let sumup = a / x1 * y1 * z;
+
+console.log(sumup)
+
+
+
+
+
